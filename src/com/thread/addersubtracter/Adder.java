9fce07721270acf72c9679c24d00912f51ce4432
ANUSRUTA DUTTA <anusruta.dutta@gmail.com>
2022-08-09 10:08:23 +0530

    Implements Runnable

--- src/com/thread/addersubtracter/Adder.java
+++ src/com/thread/addersubtracter/Adder.java
@@ -1,14 +1,25 @@
 package com.thread.addersubtracter;
 
-public class Adder {
+
+public class Adder implements Runnable{
     private Count count;
     public Adder(Count count){
         this.count = count;
     }
 
     public void add(){
+
+    }
+
+    @Override
+    public void run() {
         for (int i = 0; i < 100; i++){
             int currentValue = count.getValue();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             int nextValue = currentValue + i;
             count.setValue(nextValue);
         }
